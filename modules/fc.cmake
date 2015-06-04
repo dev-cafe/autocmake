a9e14970309c628ca2eaafa886d67c2b96c121f0
Radovan Bast <bast@users.noreply.github.com>
2015-06-04 17:57:18 +0200

    FCFLAGS overrules other flags

--- modules/fc.cmake
+++ modules/fc.cmake
@@ -1,13 +1,22 @@
 #.rst:
 #
 # Adds Fortran support.
-# Appends EXTRA_Fortran_FLAGS to CMAKE_Fortran_FLAGS.
-# If environment variable FCFLAGS is set, then these flags are used
-# and no other flags are appended.
+# Appends EXTRA_FCFLAGS to CMAKE_Fortran_FLAGS.
+# If environment variable FCFLAGS is set, then the FCFLAGS are used
+# and no other flags are used or appended.
 #
 # Defines::
 #
 #   CMAKE_Fortran_MODULE_DIRECTORY (${PROJECT_BINARY_DIR}/include/fortran)
+#
+# Example autocmake.cfg entry::
+#
+#   [fc]
+#   source: https://github.com/scisoft/autocmake/raw/master/modules/fc.cmake
+#   docopt: --fc=<FC> Fortran compiler [default: gfortran].
+#           --extra-fc-flags=<EXTRA_FCFLAGS> Extra Fortran compiler flags [default: ''].
+#   export: 'FC=%s' % arguments['--fc']``
+#   define: '-DEXTRA_FCFLAGS=%s' % arguments['--extra-fc-flags']
 
 enable_language(Fortran)
 
@@ -21,8 +30,8 @@ if(NOT CMAKE_Fortran_COMPILER_WORKS)
     message(FATAL_ERROR "CMAKE_Fortran_COMPILER_WORKS is false!")
 endif()
 
-if(DEFINED EXTRA_Fortran_FLAGS)
-    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${EXTRA_Fortran_FLAGS}")
+if(DEFINED EXTRA_FCFLAGS)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${EXTRA_FCFLAGS}")
 endif()
 
 if(DEFINED ENV{FCFLAGS})
