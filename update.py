89b68a7abdbc37dd3f3c3e4673845d8449f3240f
Miro Ilias <Miroslav.Ilias@umb.sk>
2015-06-26 10:28:18 +0200

    fix of path separator on Windows

--- update.py
+++ update.py
@@ -186,7 +186,8 @@ def gen_cmakelists(config, relative_path, list_of_modules):
     s.append('\n# directory which holds enabled cmake modules')
     s.append('set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH}')
     if  sys.platform == 'win32':
-        s.append('    ${PROJECT_SOURCE_DIR}\%s)' % os.path.join(relative_path, 'modules'))
+        #miro: keep the same separator for the whole script on Windows
+        s.append('    ${PROJECT_SOURCE_DIR}/%s)' % relative_path+"/modules")
     else:
         s.append('    ${PROJECT_SOURCE_DIR}/%s)' % os.path.join(relative_path, 'modules'))
     s.append('\n# included cmake modules')
