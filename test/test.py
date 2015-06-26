6daad775b14636ec6ee7493b2a9f9bab60b33f4a
Miro Ilias <Miroslav.Ilias@umb.sk>
2015-06-26 18:06:57 +0200

    tests are working on Windows!

--- test/test.py
+++ test/test.py
@@ -31,7 +31,7 @@ def test_cxx():
     os.chdir(os.path.join(HERE, 'cxx', 'build'))
     if  sys.platform == 'win32':
         stdout, stderr = exe('mingw32-make')
-        stdout, stderr = exe('.\bin\example.exe')
+        stdout, stderr = exe('bin\\\example.exe')
     else:
         stdout, stderr = exe('make')
         stdout, stderr = exe('./bin/example')
@@ -55,7 +55,7 @@ def test_fc():
     os.chdir(os.path.join(HERE, 'fc', 'build'))
     if  sys.platform == 'win32':
         stdout, stderr = exe("mingw32-make")
-        stdout, stderr = exe(".\bin\example.exe")
+        stdout, stderr = exe('bin\\\example.exe')
     else:
         stdout, stderr = exe('make')
         stdout, stderr = exe('./bin/example')
