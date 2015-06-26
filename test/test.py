d383b597eb243f743d90673e507475d85a8fe0fd
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-06-26 20:27:05 +0200

    fix update - must point to my fork !

--- test/test.py
+++ test/test.py
@@ -16,12 +16,12 @@ def exe(command):
 
 def test_cxx():
     os.chdir(os.path.join(HERE, 'cxx', 'cmake'))
-    update.fetch_url('https://github.com/scisoft/autocmake/raw/master/update.py', 'update.py')
+    update.fetch_url('https://github.com/miroi/autocmake/raw/master/update.py', 'update.py')
     stdout, stderr = exe('python update.py --self')
     stdout, stderr = exe('python update.py ..')
     os.chdir(os.path.join(HERE, 'cxx'))
     if  sys.platform == 'win32':
-        stdout, stderr = exe('python setup.py --cxx=g++ --generator=\"MinGW Makefiles\" ')
+        stdout, stderr = exe('python setup.py --cxx=g++ --generator="MinGW Makefiles" ')
     else:
         stdout, stderr = exe('python setup.py --cxx=g++')
     os.chdir(os.path.join(HERE, 'cxx', 'build'))
@@ -36,7 +36,7 @@ def test_cxx():
 
 def test_fc():
     os.chdir(os.path.join(HERE, 'fc', 'cmake'))
-    update.fetch_url('https://github.com/scisoft/autocmake/raw/master/update.py', 'update.py')
+    update.fetch_url('https://github.com/miroi/autocmake/raw/master/update.py', 'update.py')
     stdout, stderr = exe('python update.py --self')
     stdout, stderr = exe('python update.py ..')
     os.chdir(os.path.join(HERE, 'fc'))
