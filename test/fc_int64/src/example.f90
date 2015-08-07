e9182ffceca6e6a91c783ca954b061ba6d2637c3
Radovan Bast <bast@users.noreply.github.com>
2015-08-07 16:49:14 +0200

    cosmetics

--- test/fc_int64/src/example.f90
+++ test/fc_int64/src/example.f90
@@ -1,14 +1,22 @@
 program example
+
    implicit none
-   ! max i*4 is 2,147,483,647, add 1 for i*8
-   integer*8, parameter :: i8ref = 2147483648
+
+   ! max integer(4) is 2147483647
+   integer(8), parameter :: i8ref = 2147483648
+
    integer :: i
+
    logical :: test_ok
-   i=i8ref
+
+   i = i8ref
+
    test_ok = (sizeof(i) == 8 .and. i == 2147483648)
+
    if (test_ok) then
       print *, "PASSED"
    else
       stop "FAILED"
    endif
+
 end program
