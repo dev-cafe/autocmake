a77125f6f10ee016bcab6c3b278c2752d2d91340
Radovan Bast <bast@users.noreply.github.com>
2015-09-10 08:29:11 +0200

    rm whitespace

--- modules/math_libs.cmake
+++ modules/math_libs.cmake
@@ -509,7 +509,7 @@ if (ENABLE_STATIC_LINKING)
     endif()
     if (LAPACK_TYPE MATCHES MKL OR
         BLAS_TYPE MATCHES MKL)
-        # fix for MKL static linking 
+        # fix for MKL static linking
         set(MATH_LIBS ${MATH_LIBS} -ldl)
     endif()
 endif()
