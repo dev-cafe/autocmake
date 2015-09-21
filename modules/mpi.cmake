11137ffaf288f2712454aafda53dd543c70e20ed
Radovan Bast <bast@users.noreply.github.com>
2015-09-21 18:10:53 +0200

    add definition ENABLE_MPI

--- modules/mpi.cmake
+++ modules/mpi.cmake
@@ -34,6 +34,8 @@ if(ENABLE_MPI AND NOT MPI_FOUND)
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MPI_COMPILE_FLAGS}")
         endif()
         include_directories(${MPI_INCLUDE_PATH})
+
+        add_definitions(-DENABLE_MPI)
     else()
         message(FATAL_ERROR "-- You asked for MPI, but CMake could not find any MPI installation, check $PATH")
     endif()
