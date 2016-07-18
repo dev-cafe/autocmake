8ddd1a5f051c0676d81d55da9b4875911c879a06
Radovan Bast <bast@users.noreply.github.com>
2016-07-18 15:09:30 +0200

    rearrange

--- doc/conf.py
+++ doc/conf.py
@@ -15,16 +15,14 @@
 import sys
 import os
 
-sys.path.append(os.path.relpath(os.path.abspath('.')))
-import extract_rst
-
-sys.path.append(os.path.relpath(os.path.abspath('..')))
-from autocmake import __version__ as _version
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
+import extract_rst
+
+sys.path.insert(0, os.path.abspath('..'))
+from autocmake import __version__ as _version
 
 # -- General configuration ------------------------------------------------
 
