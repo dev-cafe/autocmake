69927f195d29aaa3f3c3c8c649914869f9154d2d
Radovan Bast <bast@users.noreply.github.com>
2016-07-18 15:11:43 +0200

    dry

--- doc/conf.py
+++ doc/conf.py
@@ -228,7 +228,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'Autocmake.tex', u'Autocmake Documentation',
-   u'Radovan Bast and Jonas Juselius', 'manual'),
+   author, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
