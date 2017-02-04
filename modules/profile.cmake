c79cbda4bd8dfbde4773cf46e81a0c995d3b8be8
Radovan Bast <bast@users.noreply.github.com>
2017-02-04 17:36:10 +0100

    add module for profiling flags

---
+++ modules/profile.cmake
@@ -0,0 +1,30 @@
+#.rst:
+#
+# Enable profiling by appending corresponding compiler flags.
+#
+# Variables modified (provided the corresponding language is enabled)::
+#
+#   CMAKE_Fortran_FLAGS
+#   CMAKE_C_FLAGS
+#   CMAKE_CXX_FLAGS
+#
+# autocmake.yml configuration::
+#
+#   docopt: "--profile Enable profiling [default: False]"
+#   define: "'-DENABLE_PROFILING={0}'.format(arguments['--check'])"
+
+option(ENABLE_PROFILING "Enable profiling" OFF)
+
+message(STATUS "Enable profiling: ${ENABLE_PROFILING}")
+
+if(ENABLE_PROFILING)
+    if(DEFINED CMAKE_Fortran_COMPILER_ID)
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -pg")
+    endif()
+    if(DEFINED CMAKE_C_COMPILER_ID)
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -pg")
+    endif()
+    if(DEFINED CMAKE_CXX_COMPILER_ID)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -pg")
+    endif()
+endif()
