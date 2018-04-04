98f468e6a6141ae47169a171fa8083c2406dbaed
Radovan Bast <bast@users.noreply.github.com>
2018-04-04 19:23:20 +0200

    refactor safeguards module

--- modules/safeguards.cmake
+++ modules/safeguards.cmake
@@ -15,12 +15,10 @@ if(${PROJECT_SOURCE_DIR} STREQUAL ${PROJECT_BINARY_DIR})
     message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there.")
 endif()
 
-string(TOLOWER "${CMAKE_BUILD_TYPE}" cmake_build_type_tolower)
-string(TOUPPER "${CMAKE_BUILD_TYPE}" cmake_build_type_toupper)
+string(TOLOWER "${CMAKE_BUILD_TYPE}" _cmake_build_type_tolower)
 
-if(NOT cmake_build_type_tolower STREQUAL "debug" AND
-   NOT cmake_build_type_tolower STREQUAL "release" AND
-   NOT cmake_build_type_tolower STREQUAL "minsizerel" AND
-   NOT cmake_build_type_tolower STREQUAL "relwithdebinfo")
-    message(FATAL_ERROR "Unknown build type \"${CMAKE_BUILD_TYPE}\". Allowed values are Debug, Release, RelWithDebInfo, and MinSizeRel (case-insensitive).")
+set(_available_types "debug" "release" "minsizerel" "relwithdebinfo")
+
+if(NOT "${_cmake_build_type_tolower}" IN_LIST _available_types)
+    message(FATAL_ERROR "Unknown build type \"${CMAKE_BUILD_TYPE}\". Allowed values are: ${_available_types}.")
 endif()
